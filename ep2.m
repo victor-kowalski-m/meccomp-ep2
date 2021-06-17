395d266646029545af85cc750b9292b7c360aa4e
Victor Kowalski Martins <victor.kowalski.m@gmail.com>
2021-06-16 21:56:16 -0300

    Ifs

--- ep2.m
+++ ep2.m
@@ -7,15 +7,15 @@ dy = 1; % cm
 lambda = 1.75;% lambda de sobrerrelaxação
 tolerancia = 0.0001; % criterio de parada do erro
 limite_iters = 10000; % numero maximo de iterações
+mi0 = 4*pi*10^-7;
+miferro = 2500*mi0;
+miar = mi0;
+mibobina = mi0;
 
 rows = 20/dy; % numero de linhas da matriz
 cols = 22/dx; % numero de colunas da matriz
 A = zeros(rows, cols); % matriz inicial com zeros
 Anova = A;  % matriz com valores novos
-
-colfinal = cols-1; % ultima coluna que será calculada na matriz (contorno = 0)
-linha_inicio_bobina = 6/20*rows; % linha em que começa a bobina
-linha_fim_bobina = 18/20*rows; % linha em que termina a bobina
 
 % Calcula A por liebmann até alcançar a tolerância desejada ou bater o
 % limite de iterações
@@ -23,43 +23,59 @@ while erro_max > tolerancia && iters < limite_iters
     
     iters = iters + 1;
     erro_max = 0;
-
+        
     % Itera matriz por linha e coluna
-    for row=1:rows-1 
-        for col=1:cols-1
+    for j=1:rows-1 
+        for i=1:cols-1
+            
+            pos_x = col*dx; % Posição do ponto no eixo x
+            pos_y = row*dy; % Posição do ponto no eixo y
+            
             % Se está embaixo ou em cima da bobina externa, pula
-            if col == colfinal && (row < linha_inicio_bobina || row > linha_fim_bobina)
+            if pos_x >= 20 && (pos_y <= 4 || pos_y >= 16)
                 continue;
             end
-            
-            pos_x = row*dx;
-            pos_y = row*dy;
-            
-            % Calcula valor do ponto
-            Acalc = formuleta;
-
-            % Interior do domínio ferro ou ar
-            
-            % Interior do domínio na bobina
-
+                        
             % Fronteira vertical bobina-ferro esquerda
-            
+            if pos_x == 16 && pos_y > 4 && pos_y < 16
+               
             % Fronteira vertical bobina-ferro direita
+            elseif pos_x == 20 && pos_y > 4 && pos_y < 16        
 
             % Fronteira vertical ar-bobina
-
+            elseif pos_x == 14 && pos_y > 4 && pos_y < 16 
+            
             % Fronteira vertical ar-ferro esquerda
+            elseif pos_x == 4
             
             % Fronteira vertical ar-ferro direita
-
+            elseif (pos_y < 4 || pos_y > 16) && pos_x == 5
+            
             % Fronteira horizontal bobina-ferro cima
+            elseif pos_y == 16 && pos_x > 14 && pos_x < 16
             
             % Fronteira horizontal bobina-ferro baixo
+            elseif pos_y == 4 && pos_x > 14 && pos_x < 16
 
             % Fronteira horizontal ar-ferro cima
+            elseif pos_y == 16 && pos_x > 5 && pos_x < 14
             
             % Fronteira horizontal ar-ferro baixo
-
+            elseif pos_y == 4 && pos_x > 5 && pos_x < 14
+            
+            % Quinas bobina-ferro-ar ???
+            
+            % Interior do domínio na bobina
+            elseif pos_y > 4 && pos_y < 16 && ((pos_x > 14 && pos_x < 16) || pos_x > 20)      
+                Acalc = (A(j, i+1) + A(j, i-1) + A(j+1, i) + A(j - 1, i))/4;
+                
+            % Interior do domínio ferro ou ar                      
+            else
+                Acalc = (A(j, i+1) + A(j, i-1) + A(j+1, i) + A(j - 1, i))/4 ...
+                    + dx^2/4*(mi0*2*10^6*cos(pi*pos_y/(12*10^-2) + 8*10^5));
+                
+            end
+            
             % Calcula valor novo de A no ponto por Liebmann
             Anova(row, col) = lambda*Acalc + (1-lambda)*A(row,col);
 

930ac62b6c547f0bdd06376a3cca2cec7aeb56aa
Victor Kowalski Martins <victor.kowalski.m@gmail.com>
2021-06-27 15:49:55 -0300

    Time que ta mexendo não se ganha

--- codigo/aplica_MDF.m
+++ codigo/aplica_MDF.m
@@ -62,27 +62,29 @@ function [A, iter] = ...
     
     elseif item == "e2"
         
-        % Equação na bobina
+        % Equações na bobina
         Aij_int_bobina = equacoes{1};
-        
-        % Equações de pontos fora da bobina
-        Aij_int_fora = equacoes{2};
-        Aij_vert_fora = equacoes{3};
-        Aij_hori_fora = equacoes{4};
+        Aij_vert_bobina = equacoes{2};
+        Aij_hori_bobina = equacoes{3};
+
+        % Equações fora da bobina
+        Aij_int_fora = equacoes{4};
+        Aij_vert_fora = equacoes{5};
+        Aij_hori_fora = equacoes{6};
                         
         % Pontos dentro da bobina
-        rows_bobina = row_eq(4)+1:row_eq(16)-1;
-        cols_bobina = [col_eq(14)+1:col_eq(16)-1 col_eq(20)+1:col_eq(22)-1];
+        rows_bobina = row_eq(4):row_eq(16);
+        cols_bobina = [col_eq(14):col_eq(16) col_eq(20):col_eq(22)-1];
                 
         instantes = tempos(end)-1;
         Tempo = 0:dt:instantes*dt; % Vetor de tempos
         iter = 0; % contador de iteracoes
         
+        Ainstantes = repmat(A, [1 1 3]);
         A = repmat(A, [1 1 2]); % Guarda o estado atual e anterior da matriz A
         k = 1;
         
-        Ainstantes = repmat(A, [1 1 3]);
-%         lambda = 1.75; % lambda de sobrerrelaxação
+        lambda = 1.75; % lambda de sobrerrelaxação
         
         for t=Tempo
             
@@ -98,13 +100,36 @@ function [A, iter] = ...
                         break;
                     end
                     
-                    A(j, i, k+1) = Aij_int_bobina( ...
-                        A, j, i, k, t, ...
-                        MIx(j, i), MIy(j, i), JZ(j, i), Sigma(j, i));                                   
+                    if any([row_eq(4) row_eq(16)] == j) && ...
+                            any([col_eq(14) col_eq(16) col_eq(20)] == i)
+                        continue
+                    end
                     
+                    % Escolhe qual equação de Aij utilizar
+                    if Fronteiras(j, i) == vertical % se é fronteira vertical
+                        A(j, i, k+1) = Aij_vert_bobina( ...
+                            A, j, i, k, Tempo(k), ...
+                            MIx(j, i-1), MIy(j, i-1), JZ(j,i-1), ...
+                            MIx(j, i+1), MIy(j, i+1), JZ(j,i+1), ...
+                            Sigma(j, i-1), Sigma(j, i+1));
+
+                    elseif Fronteiras(j, i) == horizontal % se é fronteira horizontal
+                        A(j, i, k+1) = Aij_hori_bobina( ...
+                            A, j, i, k, Tempo(k), ...
+                            MIx(j-1, i), MIy(j-1, i), JZ(j-1,i), ...
+                            MIx(j+1, i), MIy(j+1, i), JZ(j+1,i), ...
+                            Sigma(j-1, i), Sigma(j+1, i));
+                    else
+                        A(j, i, k+1) = Aij_int_bobina( ...
+                            A, j, i, k, t, ...
+                            MIx(j, i), MIy(j, i), JZ(j, i), Sigma(j, i));                                   
+                    end
+                        
                 end
             end
              
+            Ak = A(:, :, k+1);
+            
             for j=2:rows-1
                 for i=2:col_eq(20)
 
@@ -113,8 +138,10 @@ function [A, iter] = ...
                         break;
                     end
                     
-                    if i > col_eq(14) && i < col_eq(16) && ...
-                            j > row_eq(4) && j < row_eq(16)
+                    if i >= col_eq(14) && i <= col_eq(16) && ...
+                        j >= row_eq(4) && j <= row_eq(16) && ...
+                        ~(any([row_eq(4) row_eq(16)] == j) && ...
+                            any([col_eq(14) col_eq(16) col_eq(20)] == i))
                         continue
                     end
                    
@@ -124,21 +151,20 @@ function [A, iter] = ...
                     
                     % Escolhe qual equação de Aij utilizar
                     if Fronteiras(j, i) == vertical % se é fronteira vertical
-                        A(j, i, k+1) = Aij_vert_fora(A, j, i, k, t,...
+                        Acalc = Aij_vert_fora(A, j, i, k, t,...
                         MIx(j,i-1), MIy(j,i-1), JZ(j,i-1), ...
                         MIx(j,i+1), MIy(j,i+1), JZ(j,i+1));
                     elseif Fronteiras(j, i) == horizontal % se é fronteira horizontal
-                        A(j, i, k+1) = Aij_hori_fora(A, j, i, k, t,...
+                        Acalc = Aij_hori_fora(A, j, i, k, t,...
                         MIx(j-1,i), MIy(j-1,i), JZ(j-1,i), ...
                         MIx(j+1,i), MIy(j+1,i), JZ(j+1,i));
                     else % se estiver em um ponto interior do domínio
-                        A(j, i, k+1) = Aij_int_fora(A, j, i, k, t,...
+                        Acalc = Aij_int_fora(A, j, i, k, t,...
                         MIx(j, i), MIy(j, i), JZ(j, i));
                     end                                   
 
-
                     % Calcula valor novo de A no ponto por Liebmann
-%                     A(j, i, k+1) = lambda*Acalc + (1-lambda)*Aold(j, i, k);
+                    A(j, i, k+1) = lambda*Acalc + (1-lambda)*A(j, i, k);
 
                 end
             end
